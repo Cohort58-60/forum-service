dc89f60eef22a7264bd381c3dff58eac096c08c2
edward <edwardlfish@gmail.com>
2025-07-02 13:35:52 +0300

    Complete Logging by AOP

--- src/main/java/ait/cohort5860/post/service/PostServiceImpl.java
+++ src/main/java/ait/cohort5860/post/service/PostServiceImpl.java
@@ -10,6 +10,7 @@ import ait.cohort5860.post.dto.exception.PostNotFoundException;
 import ait.cohort5860.post.model.Comment;
 import ait.cohort5860.post.model.Post;
 import ait.cohort5860.post.model.Tag;
+import ait.cohort5860.post.service.logging.PostLogger;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -46,6 +47,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     @Transactional
+    @PostLogger
     public void addLike(Long id) {
         Post post = postRepository.findById(id).orElseThrow(PostNotFoundException::new);
         post.addLike();
@@ -53,6 +55,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     @Transactional
+    @PostLogger
     public PostDto updatePost(Long id, NewPostDto newPostDto) {
         Post post = postRepository.findById(id).orElseThrow(PostNotFoundException::new);
         String content = newPostDto.getContent();
