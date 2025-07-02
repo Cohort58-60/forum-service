441dbf5eb187f63ce7d875107a5ce3402c76c05a
edward <edwardlfish@gmail.com>
2025-07-02 14:12:16 +0300

    Start implements validation

--- src/main/java/ait/cohort5860/accounting/controller/UserAccountController.java
+++ src/main/java/ait/cohort5860/accounting/controller/UserAccountController.java
@@ -5,6 +5,7 @@ import ait.cohort5860.accounting.dto.UserDto;
 import ait.cohort5860.accounting.dto.UserEditDto;
 import ait.cohort5860.accounting.dto.UserRegisterDto;
 import ait.cohort5860.accounting.service.UserAccountService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class UserAccountController {
     private final UserAccountService userAccountService;
 
     @PostMapping("/register")
-    public UserDto register(@RequestBody UserRegisterDto userRegisterDto) {
+    public UserDto register(@RequestBody @Valid UserRegisterDto userRegisterDto) {
         return userAccountService.register(userRegisterDto);
     }
 
